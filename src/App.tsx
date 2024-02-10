27d78e4eda31a6b2a8fbcefa2dd1fab2faf34ef7
hallowelt100 <kayra.gueler32@gmail.com>
2024-02-10 20:33:01 +0100

    Added system preference dark & light mode + top page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Avatar, Paper } from '@mui/material';
+import { Avatar, Paper, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { useMemo } from 'react';
+
+import avatar from './img/avatar.jpeg';
 
 
-function Header() {
+
+
+// function Header() {
+//   return (
+//       <div className="header">
+//         <a href='#home' className='header-button'>Home</a>
+//         <a href='#contact' className='header-button'>Contact</a>
+//       </div>
+//   )
+// }
+
+function Top() {
   return (
-      <div className="header">
-        <a href='#home' className='header-button'>Home</a>
-        <a href='#contact' className='header-button'>Contact</a>
-      </div>
+    <div className='top'>
+      <Paper elevation={12} style={{backgroundImage: 'url("./img/background.jpg")', height: '100%', borderRadius: '15px'}}>
+        <div className='top-content'>
+          <h1>Welcome!</h1>
+          <h2>To my personal website!</h2>
+        </div>
+      </Paper>
+    </div>
   )
 }
 
@@ -29,7 +46,7 @@ function About() {
       <div className='about-avatar'>
         <Avatar
           alt="hallowelt100"
-          src="https://avatars.githubusercontent.com/u/62601323?v=4"
+          src={avatar}
           sx={{ width: 200, height: 200, boxShadow: '0px 0px 20px' }}
         />
       </div>
@@ -67,13 +84,30 @@ function Projects() {
 }
 
 function App() {
+
+const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+const theme = useMemo(
+  () =>
+    createTheme({
+      palette: {
+        mode: prefersDarkMode ? 'dark' : 'light',
+      },
+    }),
+    [prefersDarkMode],
+);
+
   return (
-    <div className='main'>
-      <div className='about-projects' id='slide'>
-        <About />
-        <Projects />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className='main' id='slide-top'>
+        <Top />
+        <div className='about-projects' id='slide'>
+          <About />
+          <Projects />
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
